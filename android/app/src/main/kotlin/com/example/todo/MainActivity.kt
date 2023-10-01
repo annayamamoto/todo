5b86aa3262c07cc75083be90b1192e072fa3ca9b
annayamamoto <56916503+annayamamoto@users.noreply.github.com>
2023-10-01 11:56:03 +0900

    エラー部分コメントアウト

--- android/app/src/main/kotlin/com/example/todo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/todo/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.todo
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
+//import io.flutter.embedding.android.FlutterActivity
+//
+//class MainActivity: FlutterActivity() {
+//}
